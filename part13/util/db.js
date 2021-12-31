285015657eec0063ebbe7b65cde85b93438048ed
Aki Utoslahti <aki.utoslahti@helsinki.fi>
2021-12-31 21:02:32 +0200

    Do 13.17 - 13.18

--- part13/util/db.js
+++ part13/util/db.js
@@ -1,11 +1,32 @@
 const { Sequelize } = require("sequelize");
 const { DATABASE_URL } = require("./config");
+const Umzug = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL);
+
+const runMigrations = async () => {
+    const migrator = new Umzug({
+        storage: "sequelize",
+        storageOptions: {
+            sequelize,
+            tableName: "migrations",
+        },
+        migrations: {
+            params: [sequelize.getQueryInterface()],
+            path: `${process.cwd()}/migrations`,
+            pattern: /\.js$/,
+        },
+    });
+    const migrations = await migrator.up();
+    console.log("Migrations up to date", {
+        files: migrations.map((mig) => mig.file),
+    });
+};
 
 const connectToDatabase = async () => {
     try {
         await sequelize.authenticate();
+        await runMigrations();
     } catch (error) {
         console.log("connecting to database failed");
         return process.exit(1);
